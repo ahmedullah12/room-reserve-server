1a0eb1d289a140699e646901e3d29d575a51de23
Ahmed Ullah <gulamjoy321@gmail.com>
2024-11-15 16:33:34 +0600

    front end link change

--- src/app/modules/Booking/Booking.service.ts
+++ src/app/modules/Booking/Booking.service.ts
@@ -191,8 +191,8 @@ const confirmBookingWithStripe = async (bookingId: string) => {
     payment_method_types: ['card'],
     line_items: [lineItem],
     mode: 'payment',
-    success_url: `http://localhost:5173/checkout/${bookingId}`,
-    cancel_url: `http://localhost:5173/checkout/${bookingId}`,
+    success_url: `https://room-reserve-client.vercel.app/checkout/${bookingId}`,
+    cancel_url: `https://room-reserve-client.vercel.app/checkout/${bookingId}`,
     metadata: { bookingId },
   });
 
